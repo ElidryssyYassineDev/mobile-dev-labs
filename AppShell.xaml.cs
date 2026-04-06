b4f1d4a31786fbbf040f7212ebc392456e3578b7
ELIDRYSSYyassine <kings4breakfast69@gmail.com>
2026-04-06 23:18:34 +0100

    Project without the categories and quiz questions yet

---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(CategoriesPage), typeof(CategoriesPage));
+            Routing.RegisterRoute(nameof(QuizPage), typeof(QuizPage));
+        }
+    }
+}
